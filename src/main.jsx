a2bde8529e90acf8bdac6f852485e86ea6f800a8
ahussnain6 <143886682+ahussnain6@users.noreply.github.com>
2024-01-09 01:00:31 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './assets/global.css';
+import {AppProvider} from "./Hooks/useFetch.jsx"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <AppProvider>
     <App />
+    </AppProvider>
   </React.StrictMode>,
 )
