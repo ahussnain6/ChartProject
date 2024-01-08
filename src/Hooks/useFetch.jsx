a6a3f561f6833c6c826b5e870e689e4aeeb81b3f
ahussnain6 <143886682+ahussnain6@users.noreply.github.com>
2024-01-09 00:59:22 +0500

    Create useFetch.jsx

---
+++ src/Hooks/useFetch.jsx
@@ -0,0 +1,20 @@
+import {createContext,useContext} from "react";
+import axios from "axios";
+const URL ="https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/month/2023-01-09/2023-12-09?apiKey=r5SZ9oT9rVzsK1cgCZZpVhZSab6vl9Im"
+import {useState,useEffect} from "react";
+ const Context = createContext();
+const AppProvider =({children})=>{
+    const  [data,setData] = useState([]);
+    useEffect(()=>{
+axios.get(URL).then((result)=>setData(result.data.results)
+    )},[])
+return(
+    <Context.Provider value={{data}}> 
+    {children}
+    </Context.Provider>
+)
+}
+const useAppContext =() => {
+    return useContext(Context);
+  };
+export {useAppContext,AppProvider};
