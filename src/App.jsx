3d954969564b88742b5a2030eeb74c31bb407e84
ahussnain6 <143886682+ahussnain6@users.noreply.github.com>
2024-01-09 19:51:59 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
- import Chart from "./components/Chart";
+ import Chart from "./components/Chart/Chart.jsx";
 import './assets/global.css';
 import { Desc } from './components/Desc';
 const App = () => {
